ebda8295b57472367931698f994ade34a66b3d19
edwinsanch33 <edwinsanch33@gmail.com>
2020-09-11 16:40:54 -0400

    start contact form to send to back-end

--- src/views/LandingPage/Sections/WorkSection.js
+++ src/views/LandingPage/Sections/WorkSection.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
 
@@ -16,6 +16,26 @@ const useStyles = makeStyles(styles);
 
 export default function WorkSection() {
   const classes = useStyles();
+
+  const [info, setInfo] = useState({
+    fullName: "edwin",
+    email: "@",
+    message: "///",
+    disabled: false,
+    emailSent: null
+  });
+
+  const handleChange = (e) => {
+    const target = e.target;
+    const value = e.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name
+    setInfo({[name]: value})  
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setInfo({disabled: true})
+  };
+
   return (
     <div id="contact" className={classes.section}>
       <GridContainer justify="center">
@@ -27,10 +47,14 @@ export default function WorkSection() {
             collaboration. We will responde get back to you in a couple of
             hours.
           </h4>
-          <form>
+          <form onSubmit={handleSubmit}>
             <GridContainer>
               <GridItem xs={12} sm={12} md={6}>
                 <CustomInput
+                  inputProps={{
+                  onChange: handleChange,
+                  value: info.fullName,
+                  name: "fullName" }}
                   labelText="Your Name"
                   id="name"
                   formControlProps={{
@@ -40,6 +64,10 @@ export default function WorkSection() {
               </GridItem>
               <GridItem xs={12} sm={12} md={6}>
                 <CustomInput
+                  inputProps={{
+                  onChange: handleChange,
+                  value: info.email,
+                  name: "email" }}
                   labelText="Your Email"
                   id="email"
                   formControlProps={{
@@ -56,13 +84,17 @@ export default function WorkSection() {
                 }}
                 inputProps={{
                   multiline: true,
-                  rows: 5
+                  rows: 5,
+                  onChange: handleChange,
+                  value: info.message,
+                  name: "message"
                 }}
               />
               <GridItem xs={12} sm={12} md={4}>
-                <Button color="primary">Send Message</Button>
+                <Button color="primary" type="submit" disabled={info.disabled}>Send Message</Button>
               </GridItem>
             </GridContainer>
+            {/* {emailSent === true && <} */}
           </form>
         </GridItem>
       </GridContainer>
