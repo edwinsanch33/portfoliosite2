08ee07ed4aaf01985a04f0def8642a4050045288
edwinsanch33 <edwinsanch33@gmail.com>
2020-09-16 12:52:39 -0400

    add build folder for deployment

---
+++ client/build/precache-manifest.07a4b34543b9aea63188c556923e012d.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc08263648bcd339cb6a0caffea448c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f52e480ec6eafba6d38",
+    "url": "/static/css/main.c5a3ee5c.chunk.css"
+  },
+  {
+    "revision": "2e7f2d29e7f129c72d11",
+    "url": "/static/js/2.20bbaf1b.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/static/js/2.20bbaf1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f52e480ec6eafba6d38",
+    "url": "/static/js/main.23c66d7d.chunk.js"
+  },
+  {
+    "revision": "deaf17d855cbdaf7344d",
+    "url": "/static/js/runtime-main.47788c5d.js"
+  },
+  {
+    "revision": "ad3d060bbca6e4de8bb065a482626299",
+    "url": "/static/media/Cleats:field.ad3d060b.jpg"
+  },
+  {
+    "revision": "a190a2724d297e25ba8dedf4185804f8",
+    "url": "/static/media/Fixie.a190a272.jpg"
+  },
+  {
+    "revision": "5e3ee71ee8c3b799a7af31374a97ef15",
+    "url": "/static/media/FromUstoYou.5e3ee71e.png"
+  },
+  {
+    "revision": "8abb9bcdadab3ed5b841fc606f902b62",
+    "url": "/static/media/GPPHeadshot.8abb9bcd.jpg"
+  },
+  {
+    "revision": "712341291b21059eec31f869f0df2da1",
+    "url": "/static/media/GamepointLanding.71234129.png"
+  },
+  {
+    "revision": "6c2afb8413277c4b157acdf7779eb82c",
+    "url": "/static/media/Snowmass.6c2afb84.jpg"
+  },
+  {
+    "revision": "3074f6b39a959f7384dcd3bb5df25f8d",
+    "url": "/static/media/YanomamiSolo.3074f6b3.jpg"
+  },
+  {
+    "revision": "432a774e07664bdc83e517cf62f20782",
+    "url": "/static/media/YanomamiTeam.432a774e.jpg"
+  },
+  {
+    "revision": "aa91d7f1c3d33812f12f29f1c9589a50",
+    "url": "/static/media/happydog-home.aa91d7f1.png"
+  },
+  {
+    "revision": "df8fd3efcd662b64b44de07f351c838b",
+    "url": "/static/media/landing-bg.df8fd3ef.jpg"
+  },
+  {
+    "revision": "baf6b40a654b078399e93e3d9cb6d455",
+    "url": "/static/media/profile-bg.baf6b40a.jpg"
+  },
+  {
+    "revision": "5c396c847e6ca8027f3a811538a1e1e0",
+    "url": "/static/media/searchbar-home.5c396c84.png"
+  }
+]);
