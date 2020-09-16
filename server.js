7bf327add281f188edc4b2137f7af96eb6781a8a
edwinsanch33 <edwinsanch33@gmail.com>
2020-09-16 13:12:41 -0400

    changes to server

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({ extended: true}));
 app.use(cors());
 
 app.use((req,res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://www.edwin-sanchez.com/');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
